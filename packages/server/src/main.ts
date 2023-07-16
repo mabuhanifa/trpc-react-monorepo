1235bf055c4fb9da4f7d6333ea2759d7811d85b3
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-16 23:41:11 +0600

    server added as module

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,10 +1,22 @@
+import * as trpcExpress from "@trpc/server/adapters/express";
+import cors from "cors";
 import express, { Application, NextFunction, Request, Response } from "express";
+import { appRouter } from "./router";
 
 const app: Application = express();
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.json({ message: "Hello world!" });
 });
+
+app.use(cors());
+
+app.use(
+  "trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+  })
+);
 
 const PORT: number = Number(process.env.PORT) || 3000;
 
