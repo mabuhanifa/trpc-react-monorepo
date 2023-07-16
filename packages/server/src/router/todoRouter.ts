87084aa4ffa2e63012277d388b24895ab08bcb26
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-16 22:42:06 +0600

    AppRouter

--- packages/server/src/router/todoRouter.ts
+++ packages/server/src/router/todoRouter.ts
@@ -1,9 +1,24 @@
+import { z } from "zod";
 import { prisma } from "../lib/prismaClient";
 import { trpc } from "../lib/trpc";
 
 export const todoRouter = trpc.router({
   list: trpc.procedure.query(async () => {
-    const todos = await prisma.todo.findMany();
-    return todos;
+    return await prisma.todo.findMany();
   }),
+  create: trpc.procedure
+    .input(
+      z.object({
+        title: z.string(),
+      })
+    )
+    .mutation(({ input }) => {
+      const title = input.title;
+      return prisma.todo.create({
+        data: {
+          title: title,
+          isCompleted: false,
+        },
+      });
+    }),
 });
