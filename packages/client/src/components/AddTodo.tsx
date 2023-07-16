bee4381ef699b0ce3c54a015c8256485f234bc19
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-17 03:11:12 +0600

    created todos

--- packages/client/src/components/AddTodo.tsx
+++ packages/client/src/components/AddTodo.tsx
@@ -1,14 +1,38 @@
+import { useState } from "react";
+import { trpc } from "../lib/trpc";
+
 export default function AddTodo() {
+  const [title, setTitle] = useState("");
+  const addTodoMutation = trpc.todo.create.useMutation();
+  const trpcContext = trpc.useContext();
+  const handleSubmit = () => {
+    addTodoMutation.mutate(
+      { title },
+      {
+        onSuccess: () => {
+          trpcContext.todo.list.invalidate();
+        },
+      }
+    );
+
+    setTitle("");
+  };
+
   return (
-    <div className='flex justify-between space-x-4'>
+    <div className="flex justify-between space-x-4">
       <input
-        type='text'
-        placeholder='Get milk...'
-        className='flex-grow rounded-md'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        type="text"
+        placeholder="Get milk..."
+        className="flex-grow rounded-md"
       />
-      <button className='bg-blue-500 hover:bg-blue-600 active:bg-blue-500 text-white py-1 px-3 rounded-md'>
+      <button
+        className="bg-blue-500 hover:bg-blue-600 active:bg-blue-500 text-white py-1 px-3 rounded-md"
+        onClick={handleSubmit}
+      >
         Add todo
       </button>
     </div>
-  )
+  );
 }
