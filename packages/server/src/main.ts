a4bb3007e61d4757fd3bca63e9c13b06fc2bab16
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-16 21:51:41 +0600

    re added

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,23 +1,10 @@
 import express, { Application, NextFunction, Request, Response } from 'express'
-import * as trpcExpress from '@trpc/server/adapters/express'
-import { appRouter } from './router'
-import cors from 'cors'
-import { createContext } from './lib/trpc'
 
 const app: Application = express()
-app.use(cors())
 
-// app.get('/', (req: Request, res: Response, next: NextFunction) => {
-//   res.json({ message: 'Hello world!' })
-// })
-
-app.use(
-  '/trpc',
-  trpcExpress.createExpressMiddleware({
-    router: appRouter,
-    createContext: createContext,
-  })
-)
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.json({ message: 'Hello world!' })
+})
 
 const PORT: number = Number(process.env.PORT) || 3000
 app.listen(PORT, () => {
